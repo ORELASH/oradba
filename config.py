13a700c089a45b89016126d21a76ed4049244f40
ORELASH <40573050+ORELASH@users.noreply.github.com>
2025-08-19 11:49:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+REDSHIFT_CONFIG = {
+    "host": "your-cluster.redshift.amazonaws.com",
+    "port": 5439,
+    "dbname": "dev",
+    "user": "your_username",
+    "password": "your_password"
+}
+
+DATASHARE_NAME = "my_database_share"
